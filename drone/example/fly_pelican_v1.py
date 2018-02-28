4fd9910009d2a91d585dac4d114993be83a2206f
stianmyd <stianmyd@stud.ntnu.no>
2018-02-28 14:44:01 +0100

    Update fly_pelican_v1.py

--- drone/example/fly_pelican_v1.py
+++ drone/example/fly_pelican_v1.py
@@ -85,7 +85,7 @@ vehicle.simple_goto(a_location1)
 
 while get_distance_metres(vehicle.location.global_relative_frame, a_location1) >= 0.3:
     print " Flying to specified location. Current altitude: ", vehicle.location.global_relative_frame.alt, "m",\
-    "  Lateral distance to target: ", "%.3f" % get_distance_metres(vehicle.location.global_relative_frame, a_location1), "m"
+    "  Lateral distance to target: ", "%.2f" % get_distance_metres(vehicle.location.global_relative_frame, a_location1), "m"
     time.sleep(1)
 print "Reaced target location"
 
@@ -95,7 +95,7 @@ vehicle.simple_goto(a_location2)
 
 while get_distance_metres(vehicle.location.global_relative_frame, a_location2) >= 0.3: 
     print "Flying to specified location. Current altitude: ", vehicle.location.global_relative_frame.alt, "m",\
-    "  Lateral distance to target: ", get_distance_metres(vehicle.location.global_relative_frame, a_location2), "m"
+    "  Lateral distance to target: ", "%.2f" % get_distance_metres(vehicle.location.global_relative_frame, a_location2), "m"
     time.sleep(1)
 print "Reaced target location"
 
@@ -104,7 +104,7 @@ vehicle.simple_goto(a_location3)
 
 while get_distance_metres(vehicle.location.global_relative_frame, a_location3) >= 0.3:
     print "Flying to specified location. Current altitude: ", vehicle.location.global_relative_frame.alt, "m",\
-    " Lateral distance to target: ", get_distance_metres(vehicle.location.global_relative_frame, a_location3), "m"
+    " Lateral distance to target: ", "%.2f" % get_distance_metres(vehicle.location.global_relative_frame, a_location3), "m"
     time.sleep(1)
 print "Reaced target location"
 
