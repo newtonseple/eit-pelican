0790cafb39557caeb0d657cbc222f4b0f9ce5c3c
stianmyd <stianmyd@stud.ntnu.no>
2018-03-07 14:46:18 +0100

    Update coarse_search.py

--- drone/example/coarse_search.py
+++ drone/example/coarse_search.py
@@ -1,6 +1,17 @@
 import math
 import dronekit
 import numpy
+import time
+
+#Set up option parsing to get connection string
+import argparse  
+parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
+parser.add_argument('--connect', 
+                   help="Vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+
+connection_string = args.connect
+sitl = None
 
 #Start SITL if no connection string specified
 if not connection_string:
