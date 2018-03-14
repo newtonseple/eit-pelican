e864c872af6dbb9b07c1af6b409e218767326dc9
Your Name <you@example.com>
2018-03-14 16:44:31 +0100

    Barryvox class, much interpret signal

--- utilities/fft-plotter.py
+++ utilities/fft-plotter.py
@@ -14,14 +14,89 @@ import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
 
+class Barryvox:
+    # TODO unify state in a single model.
+    bins = {6:"turn",
+            9:"normal",
+            11:"close",
+            12:"close",
+            13:"close",
+            14:"close",
+            15:"close",
+            16:"close",
+            17:"close",
+            20:"ahead",
+            #12:"error",
+            }
+    bins_distance = {6:8,
+            9:7,
+            11:6,
+            12:5,
+            13:4,
+            14:3,
+            15:2,
+            16:1,
+            17:0,
+            20:7,
+            #12:"error",
+            }
+    def __init__(self, tty, baud):
+        self.state = "nosignal"
+        self.distance = 7 # 7 = normal, 6-0 = close
+        self.ser = serial.Serial(tty,baud)
+        self.spectrum = np.array(np.zeros(128), dtype=np.float32)
+        self.timer = 0;
+    def set_state(self, newstate):
+        if self.state != newstate:
+            self.state = newstate
+            #print("STATE CHANGE: " + self.state)
+    def process_signal(self, bin, amplitude):
+        if bin in self.bins:
+            self.set_state(self.bins[bin])
+        if bin in self.bins_distance:
+            self.distance = self.bins_distance[bin]
+            
+        print("Status: " + str(self.get_status()))
+    
+    def check_serial(self):
+        line = self.ser.readline()
+        self.spectrum = np.array(line.split(), dtype=np.float32)
+        if np.max(self.spectrum) > 0.05:
+            self.process_signal(np.argmax(self.spectrum),np.max(self.spectrum))
+            self.timer = 0; # Reset timer for no signal
+            #print (str(np.argmax(nums)) + "\t" +  str(np.max(nums)))
+        elif self.timer > 2.0 / 0.020: # Timeout, go to nosignal
+            self.set_state("nosignal")
+        else:
+            self.timer += 1
+    def spin(self):
+        while(1):
+            self.check_serial()
+    def get_status(self):
+        # returns (bool signal, int distance)
+        if self.state in ("normal", "close", "ahead", "turn"):
+            signal = True
+            distance = self.distance
+        else:
+            signal = False
+            distance = -1
+        return (signal, distance)
+
+
+barryvox = Barryvox("COM5",115200)
+
 pointsY0 = [];
 pwnd = pg.plot();
 curveY0 = pwnd.plot(pen="y", antialias=True);
 
-ser = serial.Serial("COM5",115200);
 while(1):
     QtGui.QApplication.processEvents();
-    line = ser.readline();
-    nums = np.array(line.split());
-    curveY0.setData(nums);
-    
+    barryvox.check_serial()
+    curveY0.setData(barryvox.spectrum);
+            
+
+# snu 6bin
+# vanlig 9bin (0.1amp siden, 0.3amp vanlig)
+# nær 9-17bin
+# rett frem 20bin 
+# error 12bin 0.52amp
