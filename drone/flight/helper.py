b370084d30d37846adc7c6d15e6c8d883ab514c5
kahole <kristian@hole.priv.no>
2018-03-21 10:09:00 +0100

    added near-search module

--- drone/flight/helper.py
+++ drone/flight/helper.py
@@ -1,5 +1,5 @@
-import math
 from dronekit import LocationGlobalRelative as Pos
+import math
 
 def generate_area(coords, alt):
     return (Pos(coords[0], coords[1], alt),
@@ -8,14 +8,7 @@ def generate_area(coords, alt):
             Pos(coords[6], coords[7], alt) )
 
 def get_distance_metres(aLocation1, aLocation2):
-    """
-    Returns the ground distance in metres between two `LocationGlobal` or `LocationGlobalRelative` objects.
-    This method is an approximation, and will not be accurate over large distances and close to the
-    earth's poles. It comes from the ArduPilot test code:
-    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
-    """
+    # https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
     dlat = aLocation2.lat - aLocation1.lat
     dlong = aLocation2.lon - aLocation1.lon
     return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
-
-
